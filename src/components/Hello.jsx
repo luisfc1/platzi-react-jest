6561b887e74e034f308b63b93bd96ef589b79664
Luis Fernando <luiscaute4@gmail.com>
2021-07-14 22:52:07 -0500

    decimoquinto video: proyecto para React JS

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-const Hello = ({ title }) => (
-  <h1>{title}</h1>
+const Hello = ({ title }) => ( <
+    h1 > { title } < /h1>
 );
 
 Hello.propTypes = {
-  title: PropTypes.string
+    title: PropTypes.string
 };
 
 export default Hello;
